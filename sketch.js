2296cdb595de9810458ef04e72acd30597a2791e
nat1508 <florn640@newschool.edu>
2015-10-09 16:37:21 -0400

    Quiz

--- sketch.js
+++ sketch.js
@@ -1,16 +1,38 @@
 function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
+
 }
 
 function draw() {
-	// clear the background
-	background(50, 50, 50);
 
-	// set the drawing style
-	fill(100, 100, 100);
-	stroke(255, 255, 255);
+	//flashing lights
+	if (mouseIsPressed) {
+		background(random(0, 100), random(0, 1000), random(0, 300));
 
-	// draw an ellipse
-	ellipse(320, 180, 100, 100);
+	} else {
+		background(15, 28, 66);
+	}
+
+	//draw forest
+
+	noStroke();
+	fill(40, 87, 60);
+	rect(0, 280, 640, 100);
+	fill(0, 46, 1);
+	rect(0, 240, 640, 40);
+	triangle(150, 245, 178, 190, 206, 245);
+
+	// draw UFO
+	fill(170, 179, 182);
+	ellipse(mouseX, mouseY, 60, 80);
+	ellipse(mouseX, mouseY + 15, 100, 50);
+
+	//flashing UFO shadow
+
+	if (mouseIsPressed) {
+		fill(random(0, 100), random(0, 1000), random(0, 500));
+		ellipse(mouseX, 300, 70, 20);
+	}
+
 }
